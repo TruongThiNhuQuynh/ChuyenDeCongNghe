473011217d6ce585bed6e59f57deb7cafef0eb04
TruongThiNhuQuynh <quynhtruong272@gmail.com>
2025-08-28 22:21:20 +0700

    Cập nhật migration

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeView, DemoFieldsListView
+from .views import HomeView, DemoFieldsListView, get_name
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('demo/', DemoFieldsListView.as_view(), name='demo_list'),
+    path('forms/', get_name, name='get_name'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -12,6 +12,10 @@
 from django.views.generic import TemplateView, ListView
 from django.http import HttpResponse
 from .models_demo import DemoFields
+from django import forms
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from forms.forms import NameForm
 
 class HomeView(TemplateView):
     template_name = "home.html"
@@ -20,3 +24,13 @@ class DemoFieldsListView(ListView):
     model = DemoFields
     template_name = "demo_list.html"
     context_object_name = "objects"
+
+def get_name(request):
+    if request.method == "POST":
+        form = NameForm(request.POST)
+        if form.is_valid():
+            # Xử lý dữ liệu ở đây nếu cần
+            return HttpResponseRedirect("/polls/thanks/")
+    else:
+        form = NameForm()
+    return render(request, "name.html", {"form": form})
